74237a074daf6cba1b936376880a90609056cd2e
nachofazah <ignaciomartin.fazahbeiroa@estudiantes.unahur.edu.ar>
2025-11-03 21:34:26 -0300

    transcion lenta en el modo oscuro

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -44,133 +44,137 @@ const Home = () => {
   };
     
   return (
-    <Container fluid className="mt-0">
-          <div
-            className="banner text-center"
+    <>
+      <div
+        className="banner text-center"
+      >
+        ¡Bienvenido a Asocial!
+      </div>
+      <Container fluid className="mt-0">
+        <Row>
+          {/* Izquierda: inicio y perfil */}
+          <Col md={3} className={`border-end overflow-auto ${
+              darkMode ? "bg-dark text-light" : "bg-light text-dark"
+            }`}
           >
-            ¡Bienvenido a Asocial!
-          </div>
-      <Row>
-        {/* Izquierda: inicio y perfil */}
-        <Col md={3} className={`border-end overflow-auto ${
-            darkMode ? "bg-dark text-light" : "bg-light text-dark"
-          }`}
-        >
-          <div className="d-flex flex-column gap-2 pt-3 p-3">
+            <div className="d-flex flex-column gap-2 pt-3 p-3">
 
-            <Button
-              variant={darkMode ? "secondary" : "warning"}
-              size="lg"
-              className="bg-botones mb-2 w-50 d-block mx-auto"
-              onClick={toggleDarkMode}
-            >
-              {darkMode ? "Modo claro" : "Modo oscuro"}
-            </Button>
-            <Nav.Link href="/">
-              <Button size="lg" className="bg-botones mb-2 w-50 d-block mx-auto">
-                🏠 Inicio
+              <Button
+                variant={darkMode ? "secondary" : "warning"}
+                size="lg"
+                className="bg-botones mb-2 w-50 d-block mx-auto"
+                onClick={toggleDarkMode}
+              >
+                {darkMode ? "Modo claro" : "Modo oscuro"}
               </Button>
-            </Nav.Link>
+              <Nav.Link href="/">
+                <Button size="lg" className="bg-botones mb-2 w-50 d-block mx-auto">
+                  🏠 Inicio
+                </Button>
+              </Nav.Link>
 
-            <Nav.Link href="/">
-              <Button size="lg" className="bg-botones mb-2 w-50 d-block mx-auto">
-                👤 Perfil
-              </Button>
-            </Nav.Link>
+              <Nav.Link href="/">
+                <Button size="lg" className="bg-botones mb-2 w-50 d-block mx-auto">
+                  👤 Perfil
+                </Button>
+              </Nav.Link>
 
-          </div>
-        </Col>
+            </div>
+          </Col>
 
-        {/* Centro: publicaciones */}
-        <Col md={6} className={`border-end overflow-auto ${
+          {/* Centro: publicaciones */}
+          <Col md={6} className={`border-end overflow-auto ${
+              darkMode ? "bg-dark text-light" : "bg-light text-dark"
+            }`}
+          >
+            <div className="p-3">
+              <h4 className={`text-center mb-2 ${darkMode ? "text-light" : "text-dark"}`}>¿Qué querés compartir?</h4>
+              <Nav.Link href="/">
+                <Button variant="primary" className="bg-botones w-50 mb-3 d-block mx-auto">
+                  Crear nueva publicación
+                </Button>
+              </Nav.Link>
+            </div>
+
+            <div className="p-3">
+              <h4 className={`text-center mb-2 ${darkMode ? "text-light" : "text-dark"}`}>Publicaciones recientes</h4>
+              {posts.length === 0 ? (
+                <p>No hay publicaciones todavía.</p>
+              ) : (
+                [...posts]
+                  .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+                  .map((post) => (
+                    <Card key={post.id} className="mb-3 bg-publicaciones text-light w-50 d-block mx-auto">
+                    <Card.Body>
+                      <Card.Text>{post.description}</Card.Text>
+
+                      {/* Imágenes */}
+                      {postImages[post.id]?.map((imgUrl, idx) => (
+                        <img key={idx} src={imgUrl} alt={`Post ${post.id}`} className="img-fluid mb-2" />
+                      ))}
+
+
+                      {/* Tags */}
+                      {post.Tags && post.Tags.length > 0 && (
+                        <div className="mb-2">
+                          <h5>Etiquetas:</h5>
+                          {post.Tags.map((tag) => (
+                            <span key={tag.id} className="badge bg-secondary me-1">
+                              {tag.name}
+                            </span>
+                          ))}
+                        </div>
+                      )}
+
+                      {/* Cantidad de comentarios */}
+                      <p>
+                        {commentsCount[post.id] ?? 0} comentarios
+                      </p>
+
+                      {/* Botón Ver más */}
+                      <Button
+                        className="bg-botones"
+                        size="sm"
+                        href={`/`} // ruta dinámica
+                      >
+                        Ver más
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                ))
+              )}
+            </div>
+          </Col>
+
+          {/* Derecha: usuarios */}
+          <Col md={3} className={`border-end overflow-auto ${
             darkMode ? "bg-dark text-light" : "bg-light text-dark"
-          }`}
-        >
-          <div className="p-3">
-            <h4 className={`text-center mb-2 ${darkMode ? "text-light" : "text-dark"}`}>¿Qué querés compartir?</h4>
-            <Button variant="primary" className="bg-botones w-50 mb-3 d-block mx-auto">
-              Crear nueva publicación
-            </Button>
-          </div>
+            }`}
+          >
+            <div className="pt-3 p-3">
+              <h4 className={`text-center mb-2 ${darkMode ? "text-light" : "text-dark"}`}>Actividad reciente</h4>
+              <ListGroup className="mb-3">
+                {[
+                  "Juan comentó en tu publicación",
+                  "Pedro creo una publicación"
+                ].map((act, idx) => (
+                  <ListGroup.Item className="bg-publicaciones" key={idx}>{act}</ListGroup.Item>
+                ))}
+              </ListGroup>
+              <ListGroup className="mb-3">
+              </ListGroup>
 
-          <div className="p-3">
-            <h4 className={`text-center mb-2 ${darkMode ? "text-light" : "text-dark"}`}>Publicaciones recientes</h4>
-            {posts.length === 0 ? (
-              <p>No hay publicaciones todavía.</p>
-            ) : (
-              [...posts]
-                .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
-                .map((post) => (
-                  <Card key={post.id} className="mb-3 bg-publicaciones text-light w-50 d-block mx-auto">
-                  <Card.Body>
-                    <Card.Text>{post.description}</Card.Text>
-
-                    {/* Imágenes */}
-                    {postImages[post.id]?.map((imgUrl, idx) => (
-                      <img key={idx} src={imgUrl} alt={`Post ${post.id}`} className="img-fluid mb-2" />
-                    ))}
-
-
-                    {/* Tags */}
-                    {post.Tags && post.Tags.length > 0 && (
-                      <div className="mb-2">
-                        <h5>Etiquetas:</h5>
-                        {post.Tags.map((tag) => (
-                          <span key={tag.id} className="badge bg-secondary me-1">
-                            {tag.name}
-                          </span>
-                        ))}
-                      </div>
-                    )}
-
-                    {/* Cantidad de comentarios */}
-                    <p>
-                      {commentsCount[post.id] ?? 0} comentarios
-                    </p>
-
-                    {/* Botón Ver más */}
-                    <Button
-                      className="bg-botones"
-                      size="sm"
-                      href={`/`} // ruta dinámica
-                    >
-                      Ver más
-                    </Button>
-                  </Card.Body>
-                </Card>
-              ))
-            )}
-          </div>
-        </Col>
-
-        {/* Derecha: usuarios */}
-        <Col md={3} className={`border-end overflow-auto ${
-          darkMode ? "bg-dark text-light" : "bg-light text-dark"
-          }`}
-        >
-          <div className="pt-3 p-3">
-            <h4 className={`text-center mb-2 ${darkMode ? "text-light" : "text-dark"}`}>Actividad reciente</h4>
-            <ListGroup className="mb-3">
-              {[
-                "Juan comentó en tu publicación",
-                "Pedro creo una publicación"
-              ].map((act, idx) => (
-                <ListGroup.Item className="bg-publicaciones" key={idx}>{act}</ListGroup.Item>
-              ))}
-            </ListGroup>
-            <ListGroup className="mb-3">
-            </ListGroup>
-
-            <h4 className={`text-center mb-2 ${darkMode ? "text-light" : "text-dark"}`}>Otros usuarios</h4>
-            <ListGroup className="bg-publicaciones">
-              {["Ana", "Luis", "Carla"].map((user, idx) => (
-                <ListGroup.Item  className="bg-publicaciones" key={idx}>{user}</ListGroup.Item>
-              ))}
-            </ListGroup>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+              <h4 className={`text-center mb-2 ${darkMode ? "text-light" : "text-dark"}`}>Otros usuarios</h4>
+              <ListGroup className="bg-publicaciones">
+                {["Ana", "Luis", "Carla"].map((user, idx) => (
+                  <ListGroup.Item  className="bg-publicaciones" key={idx}>{user}</ListGroup.Item>
+                ))}
+              </ListGroup>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
